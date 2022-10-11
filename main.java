33100dbd9f1047bb38449ee557fb80fe692c3f87
Marvish Subhas Chandra <mschandr@uci.edu>
2022-10-11 11:05:07 -0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,85 @@
+// Marvish Chandra
+
+public class scaryDecorations{
+    int MadamandMasterMacyGhostlyManorBusts;
+    int MadamMacyGhostlyManorBust;
+    int MasterMactGhostlyManorBust;
+    int RoswellAlienBabyCrashVictim;
+    int FogMachineSkeleton1000W;
+    int AirblownArchwayReaperInflatable;
+    int BeheadedBenSeveredHeadUltraRealisticHalloweenProp;
+    int CelticCrossTombstone;
+    int RIPCrossTombstone;
+    int AirblownPhantasmPumpkinReaperInflatable;
+    int JackSkellingtonAir-BlownInflatableHalloweenDecoration;
+    int ProjectionGhostAir-BlownInflatableHalloweenDecoration;
+    int DemagorgonProfessionalHalloweenCostume;
+    int HatchingAlienProfessionalHalloweenProp;
+    int DemagorgonProfessionalHalloweenProp;
+    int twentytwoinLittleCreeperMonsterMarionette;
+    int GiantSkull;
+    int CeilingFanwithSkeletonArmsSkullsandLightHauntedHouseLighting;
+    int CelingFanwith8SmallSkullsandLightHauntedHouseLighting;
+    int BagofBones-LargeHalloweenProp;
+    int GhostBust-GhostHostAnimatedHauntedProjectionProp;
+    int GhoulHangingHalloweenDecorations-PackageDeal;
+    int GargoyleDecorations-4XPackageDeal;
+    int ClownHangingHalloweenDeocrations-PackageDeal;
+    int MacabreManorDVDHauntedHouseEffectsVideo;
+    int CandelabraLightUpHauntedHouseLightingProp;
+    int ChangingPortrait-PrettyHangingHalloweenDecoration;
+    int ChangingPortrait-PappyFungusHangingHalloweenDecoration;
+    int ChangingPortrait-TwinsHangingHalloweenDecoration;
+    int ChangingPortrait-AuntHazelHalloweenDecoration;
+    int BlackCatWithTurningHeadAir-BlownInflatableHalloweenDecoration;
+    int PumpkinPatchAir-BlownInflatableHalloweenDecoration;
+    int JackSkellingtonandZeroAir-BlownInflatableHalloweenDecoration;
+    int ScaryTreeWithBlackLightEffectAir-BlownInflatableHalloweenDecoration;
+    int SkeletonandPumpkinPop-upAir-BlownInflatableHalloweenDecoration;
+    int Blacklight-48inchFixturewithBulbHauntedHouseLighting;
+    int PennywiseITCollectorsBustHalloweenDecoration-LimitedRunof30Units;
+    int GhoulHangingHalloweenDecoration-36inch;
+
+    static void scaryDeco(String[] args){
+        scaryDecorations myScarydecorations = new scaryDecorations;
+        myScarydecorations.MadamandMasterMacyGhostlyManorBusts = 221.43;
+        myScarydecorations.MadamMacyGhostlyManorBust = 110.71;
+        myScarydecorations.MasterMactGhostlyManorBust = 110.71;
+        myScarydecorations.RoswellAlienBabyCrashVictim = 116.24;
+        myScarydecorations.FogMachineSkeleton1000W = 99.64;
+        myScarydecorations.AirblownArchwayReaperInflatable = 177.14;
+        myScarydecorations.BeheadedBenSeveredHeadUltraRealisticHalloweenProp = 110.71;
+        myScarydecorations.CelticCrossTombstone = 27.67;
+        myScarydecorations.RIPCrossTombstone = 27.67;
+        myScarydecorations.AirblownPhantasmPumpkinReaperInflatable = 166.07;
+        myScarydecorations.JackSkellingtonAir-BlownInflatableHalloweenDecoration = 132.85;
+        myScarydecorations.ProjectionGhostAir-BlownInflatableHalloweenDecoration = 110.71;
+        myScarydecorations.DemagorgonProfessionalHalloweenCostume = 2657.23;
+        myScarydecorations.HatchingAlienProfessionalHalloweenProp = 941.10;
+        myScarydecorations.DemagorgonProfessionalHalloweenProp = 2214.36;
+        myScarydecorations.twentytwoinLittleCreeperMonsterMarionette = 71.96;
+        myScarydecorations.GiantSkull = 276.78;
+        myScarydecorations.CeilingFanwithSkeletonArmsSkullsandLightHauntedHouseLighting = 575.72;
+        myScarydecorations.CelingFanwith8SmallSkullsandLightHauntedHouseLighting = 442.86;
+        myScarydecorations.BagofBones-LargeHalloweenProp = 99.64;
+        myScarydecorations.GhostBust-GhostHostAnimatedHauntedProjectionProp = 2435.79;
+        myScarydecorations.GhoulHangingHalloweenDecorations-PackageDeal = 304.46;
+        myScarydecorations.GargoyleDecorations-4XPackageDeal = 1716.12;
+        myScarydecorations.ClownHangingHalloweenDeocrations-PackageDeal = 387.50;
+        myScarydecorations.MacabreManorDVDHauntedHouseEffectsVideo = 44.28;
+        myScarydecorations.CandelabraLightUpHauntedHouseLightingProp = 24.35;
+        myScarydecorations.ChangingPortrait-PrettyHangingHalloweenDecoration = 55.35;
+        myScarydecorations.ChangingPortrait-PappyFungusHangingHalloweenDecoration = 55.35;
+        myScarydecorations.ChangingPortrait-TwinsHangingHalloweenDecoration = 55.35;
+        myScarydecorations.ChangingPortrait-AuntHazelHalloweenDecoration = 55.35;
+        myScarydecorations.BlackCatWithTurningHeadAir-BlownInflatableHalloweenDecoration = 132.85;
+        myScarydecorations.PumpkinPatchAir-BlownInflatableHalloweenDecoration = 110.71;
+        myScarydecorations.JackSkellingtonandZeroAir-BlownInflatableHalloweenDecoration = 166.07;
+        myScarydecorations.ScaryTreeWithBlackLightEffectAir-BlownInflatableHalloweenDecoration = 154.99;
+        myScarydecorations.SkeletonandPumpkinPop-upAir-BlownInflatableHalloweenDecoration = 110.71;
+        myScarydecorations.Blacklight-48inchFixturewithBulbHauntedHouseLighting = 55.35;
+        myScarydecorations.PennywiseITCollectorsBustHalloweenDecoration-LimitedRunof30Units = 1328.56;
+        myScarydecorations.GhoulHangingHalloweenDecoration-36inch = 27.67;
+    }
+
+}
